ced7555da2229cd220209a0c6ea002f15bc675e3
Ccordova41 <ccordova41@gmail.com>
2018-07-11 01:20:48 -0400

    component searcbar

--- src/index.js
+++ src/index.js
@@ -3,11 +3,16 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SearchBar from './components/search_bar';
 
-const API_KEY = 'AIzaSyCcBCDdozCd3Ts2IZPdMk8FC_H6Blr8SLw'; 
+// const API_KEY = 'AIzaSyCcBCDdozCd3Ts2IZPdMk8FC_H6Blr8SLw';
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
-ReactDOM.render(<App />, document.querySelector(".container"));
+ReactDOM.render(<App />, document.querySelector('.container'));
