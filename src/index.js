7c4a94565db62a6a2328fb7dafc5371b7cd2030b
Ccordova41 <ccordova41@gmail.com>
2018-07-11 20:30:18 -0400

    added ytsearch

--- src/index.js
+++ src/index.js
@@ -3,9 +3,15 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
 import SearchBar from './components/search_bar';
 
-// const API_KEY = 'AIzaSyCcBCDdozCd3Ts2IZPdMk8FC_H6Blr8SLw';
+const API_KEY = 'AIzaSyCcBCDdozCd3Ts2IZPdMk8FC_H6Blr8SLw';
+
+YTSearch({key: API_KEY, term: 'sufboards'}, function(date) {
+  console.log(data);
+});
 
 const App = () => {
   return (
