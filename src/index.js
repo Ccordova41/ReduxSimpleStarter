83900e8ae53e8e8029e9fe89beed1b1bc4cc5d58
Ccordova41 <ccordova41@gmail.com>
2018-07-11 22:24:45 -0400

    added video list item component

--- src/index.js
+++ src/index.js
@@ -1,24 +1,35 @@
 // Create a new component . this component wil produce HTML
 // take component's geenerated html and put it in the DOM.
 
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+
 
 const API_KEY = 'AIzaSyCcBCDdozCd3Ts2IZPdMk8FC_H6Blr8SLw';
 
-YTSearch({key: API_KEY, term: 'sufboards'}, function(date) {
-  console.log(data);
-});
 
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state =  { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'doctor who'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    )
+  };
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
